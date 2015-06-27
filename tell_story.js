844ffe565135223d4e982e5c794d2cc37e1ea0b9
dichenli <dichenli@sas.upenn.edu>
2015-06-26 21:13:42 -0700

    send payment link

--- tell_story.js
+++ tell_story.js
@@ -1,3 +1,32 @@
+
+
+/**
+ * @param type: a string, value is "pay" or "charge"
+ * @param recipient: recipient name, phone number, etc.
+ * @param amount: a number in dollars
+ * @param note: string, text note
+ * @param audience: String, "public" or "private"
+ * @constructor to create a venmo payment link
+ */
+var Venmo = function(type, recipient, amount, note, audience) {
+    this.type = type;
+    this.recipient = recipient;
+    this.amount = amount;
+    this.note =  note;
+    this.audience = audience;
+    return this;
+};
+
+Venmo.baseURL = "https://venmo.com/";
+
+Venmo.prototype.getPaymentLink = function () {
+    return Venmo.baseURL + "?txn=" + this.type
+        + "&recipients=" + this.recipient
+        + "&amount=" + this.amount
+        + "&note=" + this.note
+        + "&audience=" + this.audience;
+};
+
 /**
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
  * For additional samples, visit the Alexa Skills Kit developer documentation at
@@ -187,7 +216,7 @@ function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
         card: {
             type: "Simple",
             title: "SessionSpeechlet - " + title,
-            content: "SessionSpeechlet - " + output
+            content: new Venmo("pay", "alex", 1, "for lunch", "private").getPaymentLink()
         },
         reprompt: {
             outputSpeech: {
