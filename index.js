05902fb24e5de91347e48acda0b26bc8332e521e
dichenli <dichenli@sas.upenn.edu>
2015-06-27 13:34:43 -0700

    almost done

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
 var unirest = require('unirest');
+var http = require("http");
+var https = require("https");
+
+/**
+ * getJSON:  REST get request returning JSON object(s)
+ * @param options: http options object
+ * @param callback: callback to pass the results JSON object(s) back
+ */
+exports.getJSON = function(options, onResult) {
+    console.log("rest::getJSON");
+
+    var prot = options.port == 443 ? https : http;
+    var req = prot.request(options, function(res)
+    {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            var obj = JSON.parse(output);
+            onResult(res.statusCode, obj);
+        });
+    });
+
+    req.on('error', function(err) {
+        //res.send('error: ' + err.message);
+    });
+
+    req.end();
+};
+
+//getWeather();
+//unirest.get("https://verticodelabs.p.mashape.com/newspaper/?newspaper_url=http%3A%2F%2Fcnn.com%2F")
+//    .header("X-Mashape-Key", "BX2mTryaeEmshUK4TukbvzwTWmd8p19ymcWjsnM4Li601VCWqP")
+//    .header("Accept", "application/json")
+//    .end(function (result) {
+//        console.log(result.status, result.headers, result.body);
+//    });
+
+var DateGetter = function() {
+    this.date = new Date();
+}
+
+DateGetter.prototype.getDate = function() {
+    return this.date.getDate();
+}
+
+DateGetter.prototype.getMonth = function() {
+    return this.date.getMonth() + 1;
+}
+
+//function getNews(callback) {
+//    getNewsList(function(articles) {
+//        for (url in articles) {
+//            console.log("url: " + url);
+//            console.log(/^.*?\/06\/27\/.*$/.test(url));
+//        }
+//    });
+//}
+
+
+//function getNewsList(callback) {
+//    unirest.get("https://verticodelabs.p.mashape.com/newspaper/?newspaper_url=http%3A%2F%2Fcnn.com%2F")
+//        .header("X-Mashape-Key", "BX2mTryaeEmshUK4TukbvzwTWmd8p19ymcWjsnM4Li601VCWqP")
+//        .header("Accept", "application/json")
+//        .end(function (result) {
+//            console.log(result.body['article_urls']);
+//            callback(result.body['article_urls']);
+//        });
+//}
 
 /**
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
@@ -10,6 +84,8 @@ var unirest = require('unirest');
 // etc.) The JSON body of the request is provided in the event parameter.
 exports.handler = function (event, context) {
     try {
+        console.log("event: " + JSON.stringify(event));
+        console.log("context: " + JSON.stringify(context));
         console.log("event.session.application.applicationId=" + event.session.application.applicationId);
 
         /**
@@ -84,7 +160,9 @@ function onIntent(intentRequest, session, callback) {
     } else if ("HelpIntent" === intentName) {
         getWelcomeResponse(callback);
     } else if ("HistoryTodayIntent" === intentName) {
-        getDayInThePast(callback);
+        getDayInThePastIntentHandler(intent, session, callback);
+    } else if ("NewsIntent" === intentName) {
+        getNewsIntentHandler(intent, session, callback);
     } else {
         throw "Invalid intent";
     }
@@ -106,18 +184,66 @@ function getWelcomeResponse(callback) {
     var sessionAttributes = {};
     var cardTitle = "Welcome";
     var speechOutput = "failed";
-    var speech = "Welcome to the darge app, "
-        + "To start the number trap game, please say, number trap"
-        + "To tell a fact of today in history, say tell me about today in history";
-    speechOutput = speech;
-    var repromptText = speech;
+//    var speech = "To start the number trap game, please say, start number trap game. "
+//        + "To tell a fact of today in history, say tell me about today in history"
+//        + "To tell more news, say news";
+//    speechOutput = speech;
+    var repromptText = null;
     var shouldEndSession = false;
-
-    callback(sessionAttributes,
-        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+    getWeather(function(weatherText){
+        getStock(function(stockText) {
+            getDayInThePast(function(DayInThePast) {
+                getNews(0, function(newsText, newsList) {
+                    sessionAttributes = {
+                        newsIndex: "" + 1
+                    }
+                    speechOutput = "Welcome to morning brief. " + weatherText + stockText + DayInThePast + newsText;
+                    console.log("start to populate session..." + speechOutput);
+//                    speechOutput = "ha ha";
+                    callback(sessionAttributes,
+                        buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+                });
+            });
+        });
+    });
 }
 
-function getDayInThePast(callback) {
+function getNewsIntentHandler(intent, session, callback) {
+    var numberOfNews = 1;
+    var newsIndex = 1;
+    if (session && session.attributes) {
+        newsIndex = parseInt(session.attributes.newsIndex);
+    }
+
+    console.log("new intent handler: " + newsIndex);
+    var sessionAttributes = {};
+    var speechOutput = "Top" + newsIndex + " news: ";
+    var cardTitle = "News brief";
+    var repromptText = null;
+    var shouldEndSession = false;
+
+    getNewsList(function(newsList) {
+        if (newsIndex >= newsList.length) {
+            newsIndex = 0;
+        }
+        for (var i = newsIndex;
+            i < newsIndex + numberOfNews && i < newsList.length;
+            i++) {
+
+            speechOutput += (". " + newsList[i].title
+                + ". " + newsList[i].abstract);
+        }
+        speechOutput += ". To hear more news, say news";
+        sessionAttributes = {
+            newsIndex: newsIndex + numberOfNews
+        };
+        console.log(speechOutput);
+        callback(sessionAttributes,
+            buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+    });
+}
+
+function getDayInThePastIntentHandler(intent, session, callback) {
     var date = new Date();
     var month = date.getMonth() + 1;
     var day = date.getDate();
@@ -126,24 +252,85 @@ function getDayInThePast(callback) {
     var cardTitle = "fact of day";
     var repromptText = "To begin, please say, tell me about today in history";
     var shouldEndSession = false;
-    console.log(month, day);
     unirest.get("https://numbersapi.p.mashape.com/"
         + month + "/" + day + "/date?fragment=true&json=true")
     .header("X-Mashape-Key", "BX2mTryaeEmshUK4TukbvzwTWmd8p19ymcWjsnM4Li601VCWqP")
     .header("Accept", "text/plain")
     .end(function (result) {
-        console.log(result.body.text);
         speechOutput = result.body.text;
         callback(sessionAttributes,
             buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
     });
+};
+
+function getDayInThePast(callback) {
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var dayInThePastText = ". failed. ";
+    var shouldEndSession = false;
+    unirest.get("https://numbersapi.p.mashape.com/"
+        + month + "/" + day + "/date?fragment=true&json=true")
+        .header("X-Mashape-Key", "BX2mTryaeEmshUK4TukbvzwTWmd8p19ymcWjsnM4Li601VCWqP")
+        .header("Accept", "text/plain")
+        .end(function (result) {
+            dayInThePastText = ". Today in history. " + result.body.text;
+            callback(dayInThePastText);
+        });
+};
+
+function getNewsList(callback) {
+    var optionsNYT = {
+        host: 'api.nytimes.com',
+        port: 80,
+        path: '/svc/topstories/v1/home.json?api-key=e900c3cd0f9061e3e21b76a826a05a48:4:72394500',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    exports.getJSON(optionsNYT, function(code, json) {
+        callback(json.results);
+    });
 }
 
+function getNews(index, callback) {
+    getNewsList(function(newsList) {
+//        var index = Math.floor((Math.random() * json.results.length));
+//        console.log(json.results[index].title);
+        var text = (". Today's top news. " + newsList[index].title
+            + ". " + newsList[index].abstract);
+        callback(text);
+    });
+}
+
+function getStock(callback) {
+    unirest.get("https://www.enclout.com/api/v1/yahoo_finance/show.json?auth_token=zxCxAcN7zXVHwBtn_XsE&text=AMZN")
+        .header("X-Mashape-Key", "BX2mTryaeEmshUK4TukbvzwTWmd8p19ymcWjsnM4Li601VCWqP")
+        .header("Accept", "application/json")
+        .end(function (result) {
+            var open = parseFloat(result.body[0].open);
+            var close = parseFloat(result.body[0].close);
+            var percentChange = (close - open) / open * 100;
+            var upDown = percentChange < 0 ? " traded down " : " was up ";
+            var stockText = ". Stock price. Amazon.com" + upDown + (Math.round(Math.abs(percentChange) * 100) / 100) + "% yesterday, at " + close + " dollars";
+            callback(stockText);
+        });
+}
+
+function getWeather(callback) {
+    unirest.get("https://simple-weather.p.mashape.com/weather?lat=-122.3331&lng=47.6097")
+        .header("X-Mashape-Key", "BX2mTryaeEmshUK4TukbvzwTWmd8p19ymcWjsnM4Li601VCWqP")
+        .header("Accept", "text/plain")
+        .end(function (result) {
+            callback("The weather is: " + result.body.trim() + ". ");
+        });
+}
 
 function speakNumber(low, high) {
-    console.log("speakNumber: " + low);
     return "Number trap, " + low + " to " + high;
-};
+}
 
 var DEFAULT_NUMBER = 99;
 var LOW_NUMBER = 1;
@@ -159,15 +346,13 @@ function startGame(intent, session, callback) {
     var speechOutput = "";
 
     sessionAttributes = {
-//        numberTrap: Math.floor((Math.random() * DEFAULT_NUMBER) + 1),
-        numberTrap: 50,
+        numberTrap: Math.floor((Math.random() * DEFAULT_NUMBER) + 1),
+//        numberTrap: 50,
         lowNumber: LOW_NUMBER,
         highNumber: HIGH_NUMBER
     };
     speechOutput = speakNumber(sessionAttributes.lowNumber, sessionAttributes.highNumber)
         + ", To guess a number, say, my guess number is twenty five";
-    console.log("numberTrap: " + sessionAttributes.numberTrap);
-    console.log(speechOutput);
     repromptText = speakNumber(sessionAttributes.lowNumber, sessionAttributes.highNumber)
         + ", To guess a number, say, my guess number is twenty five";
 
@@ -176,8 +361,7 @@ function startGame(intent, session, callback) {
 }
 
 function generatePunishment() {
-    return ". Truth or dare, "
-        + "Who was your first crush, or who is your current crush?";
+    return ". Truth or dare. ";
 };
 
 function guessNumber(intent, session, callback) {
